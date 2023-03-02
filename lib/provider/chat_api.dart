3005167486f75454330395ca21b883f431e68c65
Haeun <haeun930515@gmail.com>
2023-03-03 03:58:39 +0900

    폴더 구조 변경/

--- lib/provider/chat_api.dart
+++ lib/provider/chat_api.dart
@@ -2,6 +2,24 @@ import 'dart:convert';
 import '../model/chat_model.dart';
 import 'package:http/http.dart' as http;
 
+class ChatApiService {
+  static Future<List<ChatModel>> getChats() async {
+    List<ChatModel> chatInstances = [];
+    // chat api 주소에 맞게 변경 현재는 웹툰을 불러옴
+    final url =
+        Uri.parse('https://webtoon-crawler.nomadcoders.workers.dev/today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> chats = jsonDecode(response.body);
+      for (var chat in chats) {
+        chatInstances.add(ChatModel.fromJson(chat));
+      }
+      return chatInstances;
+    }
+    throw Error();
+  }
+}
+
 class ChatAPIService {
   Map<String, String> headers = {
     "Authorization": "하단 auth key 입력 후 테스트 가능",
