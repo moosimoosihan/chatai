c41689b8408d038a4e255f76e89ea6e127c1a8ab
Haeun <haeun930515@gmail.com>
2023-03-03 03:22:22 +0900

    에러 수정 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+//
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   // runApp() 호출 전 Flutter SDK 초기화
