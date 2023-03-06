a03ebfce87d9d305876b4ff62ae039c864fe76df
Haeun <haeun930515@gmail.com>
2023-03-06 14:42:51 +0900

    chat screen import 변경

--- lib/view/ui/main/sub_screens/chat_screen.dart
+++ lib/view/ui/main/sub_screens/chat_screen.dart
@@ -1,8 +1,7 @@
+import 'package:chatai/provider/firebase_api.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import '../../../../provider/firebase_api.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({
