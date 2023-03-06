900480422e6fb0f2c46f8e02b58c82a17d10a273
segun <segun408@gmail.com>
2023-03-06 14:12:54 +0900

    add commit

--- lib/model/chat_model.dart
+++ lib/model/chat_model.dart
@@ -11,10 +11,17 @@ class ChatModel {
   final String usertext;
   final String aitext;
   final int uploadTime;
+  final int roomNum;
 
   factory ChatModel.fromJson(Map<String, dynamic> json) {
-    return ChatModel(json['id'], json['name'], json['usertext'], json['aitext'],
-        json['uploadTime']);
+    return ChatModel(
+      json['id'],
+      json['name'],
+      json['usertext'],
+      json['aitext'],
+      json['uploadTime'],
+      json['roomNum'],
+    );
   }
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
