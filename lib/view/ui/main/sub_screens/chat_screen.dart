ed500eb55cc63b253c2f0a35e664c75e4106bff2
Haeun <haeun930515@gmail.com>
2023-03-06 14:42:51 +0900

    chat screen import 변경

--- lib/view/ui/main/sub_screens/chat_screen.dart
+++ lib/view/ui/main/sub_screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chatai/provider/firebase_api.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
