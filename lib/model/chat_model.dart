516c293334ffeacfc29603797d5f83e0517273b5
segun <segun408@gmail.com>
2023-03-06 14:12:54 +0900

    add commit

--- lib/model/chat_model.dart
+++ lib/model/chat_model.dart
@@ -11,6 +11,7 @@ class ChatModel {
   final String usertext;
   final String aitext;
   final int uploadTime;
+  final int roomNum;
 
   factory ChatModel.fromJson(Map<String, dynamic> json) {
     return ChatModel(
@@ -28,6 +29,7 @@ class ChatModel {
       'usertext': usertext,
       'aitext': aitext,
       'uploadTime': uploadTime,
+      'roomNum': roomNum,
     };
   }
 }
