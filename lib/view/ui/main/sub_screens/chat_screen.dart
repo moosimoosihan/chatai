c887c922a071204f192b710dc28bc54ea1a7ae71
segun <segun408@gmail.com>
2023-03-06 14:33:08 +0900

    snapshot add limit 1

--- lib/view/ui/main/sub_screens/chat_screen.dart
+++ lib/view/ui/main/sub_screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:chatai/provider/firebase_api.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
