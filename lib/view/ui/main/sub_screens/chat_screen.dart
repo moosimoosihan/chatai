7956e1dc394115f457a6b9c8cbe30ec663def017
segun <segun408@gmail.com>
2023-03-07 19:42:27 +0900

    chatroom creat and del

--- lib/view/ui/main/sub_screens/chat_screen.dart
+++ lib/view/ui/main/sub_screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chatai/provider/firebase_api.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,31 +14,52 @@ class ChatsScreen extends StatefulWidget {
 
 class ChatsScreenState extends State<ChatsScreen> {
   late int maxRoomNum;
+
   @override
   void initState() {
+    Provider.of<FirebaseService>(context, listen: false).listenToChatRooms();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     var p = Provider.of<FirebaseService>(context);
-    maxRoomNum = 10;
     return Scaffold(
       appBar: AppBar(
         title: const Text('채팅방'),
       ),
-      body: ListView.builder(
-        itemCount: maxRoomNum,
-        itemBuilder: (context, index) {
-          // 채팅방 목록을 보여주는 코드 작성
-          return ListTile(
-            onTap: () {
-              setState(() {
-                p.roomNum = index;
-              });
-            },
-            title: Text('채팅방 ${index + 1}'),
-          );
+      body: StreamBuilder<QuerySnapshot<Object?>>(
+        key: UniqueKey(),
+        stream: p.listenToChatRooms(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('로딩 실패'));
+          } else {
+            var roomTitles = snapshot.data!.docs;
+            maxRoomNum = roomTitles.length;
+            return ListView.builder(
+              itemCount: maxRoomNum,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    onTap: () {
+                      setState(() {
+                        p.roomNum = index;
+                      });
+                    },
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () => p.DelChatRoom(index),
+                    ),
+                    title: Text('채팅방 ${index + 1}'),
+                    subtitle: Text(roomTitles[index].get('usertext')),
+                  ),
+                );
+              },
+            );
+          }
         },
       ),
     );
