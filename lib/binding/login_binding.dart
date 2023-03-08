9f8030bf3b2c54e97f4d486a40ab0b0e2ff9efa2
Haeun <haeun930515@gmail.com>
2023-03-08 16:05:19 +0900

    chat_api: 미사용 코드 제거 LoginRepository: 이름 변경

--- lib/binding/login_binding.dart
+++ lib/binding/login_binding.dart
@@ -1,6 +1,6 @@
 import 'package:chatai/controller/login_controller.dart';
 import 'package:chatai/provider/kakao_api.dart';
-import 'package:chatai/repository/example_repository.dart';
+import 'package:chatai/repository/login_repository.dart';
 import 'package:get/get.dart';
 
 class LoginBinding implements Bindings {
@@ -8,7 +8,7 @@ class LoginBinding implements Bindings {
   void dependencies() {
     Get.lazyPut(() {
       return LoginController(
-          repository: ExampleRepository(apiClient: LoginApi()));
+          repository: LoginRepository(apiClient: LoginApi()));
     });
   }
 }
