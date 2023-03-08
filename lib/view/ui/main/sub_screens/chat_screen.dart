29def52e5361b9fc2e95930eb08b1b7700fb4641
segun <segun408@gmail.com>
2023-03-08 14:51:36 +0900

    chat hero and chatroom design

--- lib/view/ui/main/sub_screens/chat_screen.dart
+++ lib/view/ui/main/sub_screens/chat_screen.dart
@@ -42,20 +42,44 @@ class ChatsScreenState extends State<ChatsScreen> {
             return ListView.builder(
               itemCount: maxRoomNum,
               itemBuilder: (context, index) {
-                return Card(
-                  child: ListTile(
-                    onTap: () {
-                      setState(() {
-                        p.roomNum = index;
-                      });
-                    },
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete),
-                      onPressed: () => p.DelChatRoom(index),
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 13),
+                      child: Text(
+                        '채팅방 $index',
+                        style: const TextStyle(fontSize: 17),
+                      ),
                     ),
-                    title: Text('채팅방 ${index + 1}'),
-                    subtitle: Text(roomTitles[index].get('usertext')),
-                  ),
+                    Hero(
+                      tag: 'chathero',
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[700],
+                            borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(30),
+                                topRight: Radius.circular(30),
+                                bottomLeft: Radius.circular(30),
+                                bottomRight: Radius.circular(0))),
+                        child: ListTile(
+                          onTap: () {
+                            setState(() {
+                              p.roomNum = index;
+                            });
+                          },
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () => p.DelChatRoom(index),
+                          ),
+                          title: Text(roomTitles[index].get('usertext'),
+                              style: const TextStyle(
+                                  color: Colors.white, fontSize: 18)),
+                        ),
+                      ),
+                    ),
+                  ],
                 );
               },
             );
